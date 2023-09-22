ac62bb3690ead83d6d7f9f2503790afa8b6f0235
JAYBLA <jayblaenterprises@gmail.com>
2023-09-22 15:53:34 +0300

    static

--- mkulima_digital_api/models.py
+++ mkulima_digital_api/models.py
@@ -6,10 +6,16 @@ class Pembejeo(models.Model):
     created_at  = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 class Target(models.Model):
     name = models.CharField(max_length = 180)    
     created_at  = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Feedback(models.Model):
